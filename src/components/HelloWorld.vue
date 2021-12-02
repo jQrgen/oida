3e9d77fcba42a27e2d8f86c93e519cf7ed311881
Jørgen Svennevik Notland <jorgen.notland@protonmail.com>
2021-12-02 02:03:52 +0100

    Spotify playlist

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,15 +4,23 @@
   </div>
 
   <h2>Music</h2>
+  <div>
+    <iframe allowtransparency="true" scrolling="no" frameborder="no" src="https://w.soundcloud.com/icon/?url=http%3A%2F%2Fsoundcloud.com%2Foidamusic&color=black_white&size=24" style="width: 24px; height: 24px;"></iframe>
+    <div style="display:flex;flex-wrap:wrap"><a href="https://www.instagram.com/oidamusic/" target="_blank" rel="noopener noreferrer" style="text-decoration:none;border:0;width:23px;height:23px;padding:2px;margin:auto;color:#ffffff;border-radius:15%;background-color:#4d4d4d;"><svg class="niftybutton-instagram" style="display:block;fill:currentColor" data-tag="ins" data-name="Instagram" viewBox="0 0 512 512" preserveAspectRatio="xMidYMid meet">
+      <path d="M256 109.3c47.8 0 53.4 0.2 72.3 1 17.4 0.8 26.9 3.7 33.2 6.2 8.4 3.2 14.3 7.1 20.6 13.4 6.3 6.3 10.1 12.2 13.4 20.6 2.5 6.3 5.4 15.8 6.2 33.2 0.9 18.9 1 24.5 1 72.3s-0.2 53.4-1 72.3c-0.8 17.4-3.7 26.9-6.2 33.2 -3.2 8.4-7.1 14.3-13.4 20.6 -6.3 6.3-12.2 10.1-20.6 13.4 -6.3 2.5-15.8 5.4-33.2 6.2 -18.9 0.9-24.5 1-72.3 1s-53.4-0.2-72.3-1c-17.4-0.8-26.9-3.7-33.2-6.2 -8.4-3.2-14.3-7.1-20.6-13.4 -6.3-6.3-10.1-12.2-13.4-20.6 -2.5-6.3-5.4-15.8-6.2-33.2 -0.9-18.9-1-24.5-1-72.3s0.2-53.4 1-72.3c0.8-17.4 3.7-26.9 6.2-33.2 3.2-8.4 7.1-14.3 13.4-20.6 6.3-6.3 12.2-10.1 20.6-13.4 6.3-2.5 15.8-5.4 33.2-6.2C202.6 109.5 208.2 109.3 256 109.3M256 77.1c-48.6 0-54.7 0.2-73.8 1.1 -19 0.9-32.1 3.9-43.4 8.3 -11.8 4.6-21.7 10.7-31.7 20.6 -9.9 9.9-16.1 19.9-20.6 31.7 -4.4 11.4-7.4 24.4-8.3 43.4 -0.9 19.1-1.1 25.2-1.1 73.8 0 48.6 0.2 54.7 1.1 73.8 0.9 19 3.9 32.1 8.3 43.4 4.6 11.8 10.7 21.7 20.6 31.7 9.9 9.9 19.9 16.1 31.7 20.6 11.4 4.4 24.4 7.4 43.4 8.3 19.1 0.9 25.2 1.1 73.8 1.1s54.7-0.2 73.8-1.1c19-0.9 32.1-3.9 43.4-8.3 11.8-4.6 21.7-10.7 31.7-20.6 9.9-9.9 16.1-19.9 20.6-31.7 4.4-11.4 7.4-24.4 8.3-43.4 0.9-19.1 1.1-25.2 1.1-73.8s-0.2-54.7-1.1-73.8c-0.9-19-3.9-32.1-8.3-43.4 -4.6-11.8-10.7-21.7-20.6-31.7 -9.9-9.9-19.9-16.1-31.7-20.6 -11.4-4.4-24.4-7.4-43.4-8.3C310.7 77.3 304.6 77.1 256 77.1L256 77.1z"></path>
+      <path d="M256 164.1c-50.7 0-91.9 41.1-91.9 91.9s41.1 91.9 91.9 91.9 91.9-41.1 91.9-91.9S306.7 164.1 256 164.1zM256 315.6c-32.9 0-59.6-26.7-59.6-59.6s26.7-59.6 59.6-59.6 59.6 26.7 59.6 59.6S288.9 315.6 256 315.6z"></path>
+      <circle cx="351.5" cy="160.5" r="21.5"></circle>
+    </svg></a></div>
+
+  </div>
+  <br>
+  <a href="https://linktr.ee/oidamusic" style="color: black">Releases</a>
+  <p>
+  </p>
   <hr>
 
+  <iframe src="https://open.spotify.com/embed/playlist/4N64MJKYZ7LOexIZ47wTO9?utm_source=generator" width="100%" height="380" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
   <br>
-  <iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/924059545&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/oidamusic" title="OIDA" target="_blank" style="color: #cccccc; text-decoration: none;">OIDA</a> · <a href="https://soundcloud.com/oidamusic/dampskip" title="ROLF SAITOTI - DAMPSKIP" target="_blank" style="color: #cccccc; text-decoration: none;">ROLF SAITOTI - DAMPSKIP</a></div>  <br>
-  <iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/908710864&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/oidamusic" title="OIDA" target="_blank" style="color: #cccccc; text-decoration: none;">OIDA</a> · <a href="https://soundcloud.com/oidamusic/trist" title="GRIM PIL, FEJL &amp; DALER - TRIST" target="_blank" style="color: #cccccc; text-decoration: none;">GRIM PIL, FEJL &amp; DALER - TRIST</a></div>
-  <iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/918614806&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/oidamusic" title="OIDA" target="_blank" style="color: #cccccc; text-decoration: none;">OIDA</a> · <a href="https://soundcloud.com/oidamusic/this-feeling-deep-club" title="TIMMY AKA - THIS FEELING" target="_blank" style="color: #cccccc; text-decoration: none;">TIMMY AKA - THIS FEELING</a></div>
-  <br>
-  <iframe width="100%" height="450" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1151522213&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/oidamusic" title="OIDA" target="_blank" style="color: #cccccc; text-decoration: none;">OIDA</a> · <a href="https://soundcloud.com/oidamusic/sets/dnceprty-nyhavna" title="DNCEPRTY @NYHAVNA" target="_blank" style="color: #cccccc; text-decoration: none;">DNCEPRTY @NYHAVNA</a></div>
-  <iframe width="100%" height="450" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1150631102&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/oidamusic" title="OIDA" target="_blank" style="color: #cccccc; text-decoration: none;">OIDA</a> · <a href="https://soundcloud.com/oidamusic/sets/mixed-in-the-basement" title="MIXED BY ANNHANI" target="_blank" style="color: #cccccc; text-decoration: none;">MIXED BY ANNHANI</a></div>
 </template>
 
 <script lang="ts">
@@ -25,16 +33,6 @@ import { Options, Vue } from 'vue-class-component'
 })
 export default class HelloWorld extends Vue {
   msg!: string
-
-  mounted () {
-    const plugin = document.createElement('script')
-    plugin.setAttribute(
-      'src',
-      'https://unpkg.com/embeddable-nfts/dist/nft-card.min.js'
-    )
-    plugin.async = true
-    document.head.appendChild(plugin)
-  }
 }
 </script>
 
